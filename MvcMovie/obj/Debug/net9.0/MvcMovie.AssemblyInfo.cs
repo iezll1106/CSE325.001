9c318e5e7b9ac06499a486654b63990219897e23
iezll1106 <llizardo@byupathway.edu>
2025-11-07 13:11:52 +0800

    2nd commit

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801f1768401a9612a9a1d7ca577084e28bac1bd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe581a18421fcb953bf7985c67bc1635cfdb6c52")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
